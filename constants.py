9d1793f96cfd4dcff6b592584effd7b3036e4a1d
cakeGit <65340665+cakeGit@users.noreply.github.com>
2024-03-07 20:53:29 +0000

    Config for scoreboard cutoff

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
 RENDER_DEBUG_CHECKOUT_AREA = True
 RENDER_DEBUG_PLAYER_COLLIDERS = True
 RENDER_DEBUG_BUTTON_COLLIDERS = True
+
+#Config
+
+# How many scores can be shown at once (deletes excess)
+SCORES_MAX_LENGTH = 20
