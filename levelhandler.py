8313375240722d93a428bd7e4c422c8f9a8fd413
cakeGit <65340665+cakeGit@users.noreply.github.com>
2024-03-30 19:31:35 +0000

    Added in noew level;s

--- levelhandler.py
+++ levelhandler.py
@@ -1,7 +1,11 @@
 import levels.level1
+import levels.level2
+import levels.level3
 
 # Just a list to all the levels
 
 allLevels = [
-    levels.level1
+    levels.level1,
+    levels.level2,
+    levels.level3
 ]
