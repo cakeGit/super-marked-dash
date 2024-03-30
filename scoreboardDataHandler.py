c521859411c38c83bdc11b782836eeacefa913ea
cakeGit <65340665+cakeGit@users.noreply.github.com>
2024-03-30 17:57:48 +0000

    Changed to an object, so that each level can have its own scoreboard

--- scoreboardDataHandler.py
+++ scoreboardDataHandler.py
@@ -1,82 +1,90 @@
 import csvStreamer
 import constants
 
-filename = "scoreboard"
-scoreboardFile = csvStreamer.CSVFile(filename)
+# the start of the level filenames, scoreboard-1.csv
+filenameRoot = "scoreboard"
 
-scores = []
+class ScoreboardDataHandler():
+    def __init__(self, levelNumber):
+        self.level = levelNumber
+        self.filename = filenameRoot + "-" + levelNumber
+        self.scoreboardFile = csvStreamer.CSVFile(self.filename)
+        self.scores = []
+        self.read()
 
-# Sort the array and remove excess
-def formatScores():
-    global scores
-    scores.sort(reverse=False, key=lambda o: o[1])
-    print("Sorted scores: ", scores)
-    if len(scores) > constants.SCORES_MAX_LENGTH:
-        print("Removed excess score(s)")
-        scores = scores[0:constants.SCORES_MAX_LENGTH]
+    # Sort the array and remove excess
+    def formatScores(self):
+        # Sort
+        self.scores.sort(reverse=False, key=lambda o: o[1])
+        print("Sorted scores: ", self.scores)
 
-# Write from scoreboard.csv -> See scvStreamer.py
-def put(name, score):
-    global scores
+        # If there is more than the max, limit the list and put a note in the log
+        if len(self.scores) > constants.SCORES_MAX_LENGTH:
+            print("Removed excess score(s)")
+            self.scores = self.scores[0:constants.SCORES_MAX_LENGTH]
 
-    if (name == ""):
-        name = "Unknown"
+    # Add in a new entry to the scoreboard, sort the rankings, and save the file
+    def put(self, name, score):
+        if (name == ""):
+            name = "Unknown"
 
-    scores.append([name, score])
-    formatScores()
-    write()
+        self.scores.append([name, score])
+        self.formatScores()
+        self.write()
 
-# Write from scoreboard.csv -> See scvStreamer.py
-def write():
-    stream = scoreboardFile.openWriteStream()
+    # Write from scoreboard.csv -> See scvStreamer.py
+    def write(self):
+        stream = self.scoreboardFile.openWriteStream()
 
-    stream.writeCommentLine("Scoreboard data for Super Market Dash")
-    stream.writeCommentLine("Format is: name, score")
-    stream.writeEmptyLine()
+        stream.writeCommentLine("Scoreboard data for Super Market Dash")
+        stream.writeCommentLine("Format is: name, score")
+        stream.writeEmptyLine()
 
-    for entry in scores:
-        stream.writeFieldsLine([ entry[0], str(entry[1]) ])
-    
-    stream.close()
-    print("Saved scores to"+filename+".csv")
+        for entry in self.scores:
+            stream.writeFieldsLine([ entry[0], str(entry[1]) ])
+        
+        stream.close()
+        print("Saved scores to"+self.filename+".csv")
 
-# Read from scoreboard.csv -> See scvStreamer.py
-def read():
-    global scores
+    # Read from scoreboard.csv -> See scvStreamer.py
+    def read(self):
 
-    if not scoreboardFile.fileExists():
-        print("Tried to read user data from "+filename+".csv but file doesent exist")
-        return
-    else:
-        readScores = [] #Dont want to change the actual scores variable unless we are sure it was valid
-        readStream = scoreboardFile.openReadStream()
+        if not self.scoreboardFile.fileExists():
+            print("Tried to read user data from "+self.filename+".csv but file doesent exist")
+            return
+        else:
+            readScores = [] #Dont want to change the actual scores variable unless we are sure it was valid
+            readStream = self.scoreboardFile.openReadStream() #Open the stream
 
-        while not readStream.hasEnded():
-            pair = readStream.readFields(2)
+            while not readStream.hasEnded():
+                pair = readStream.readFields(2) #Read 2 fields at a time
 
-            if (pair[0] == "" or pair[1] == ""):
-                print("Incorrect formatting in "+filename+".csv, missing / empty fields!")
-                return
-            
-            try:
-                score = int(pair[1])
-            except ValueError:
-                print("Incorrect formatting in "+filename+".csv, score is not an integer (of time in 1/100 seconds)!")
-                return
+                #Check that the fields are not empty
+                if (pair[0] == "" or pair[1] == ""):
+                    print("Incorrect formatting in "+self.filename+".csv, missing / empty fields!")
+                    return #Cancel reading
+                
+                #The second field should be a number, so check
+                try:
+                    score = int(pair[1])
+                except ValueError:
+                    print("Incorrect formatting in "+self.filename+".csv, score is not an integer (of time in 1/100 seconds)!")
+                    return #Cancel reading
 
-            readScores.append([pair[0], score])
+                readScores.append([pair[0], score])
 
-        print("Read scores: ", readScores)
-        scores = readScores
-        formatScores()
+            print("Read scores: ", readScores)
+            self.scores = readScores
+            self.formatScores()
 
-# Return a string list to be displayed
-def getStringEntries():
-    entries = []
-    index = 1
-    for entry in scores:
-        entries.append(str(index) +  "." + entry[0] + ": " + str(entry[1] /100),)
-        index += 1
-    return entries
+    # Return a string list to be displayed
+    def getStringEntries(self):
+        entries = [] #Create a list to be returned
+        index = 1 #Track the current ranking of the entry
 
-read()
+        for entry in self.scores:
+            #Add in an entry formatted as Ranking. Name: Time
+            entries.append(str(index) +  ". " + entry[0] + ": " + str(entry[1] /100),)
+            index += 1
+
+        return entries
